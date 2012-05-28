57a8f959a4b74855c790b63c5f24061b640fc3bd
John Sakers <yzwy4mty9f@snkmail.com>
2012-05-28 02:31:35 -0400

    finish ch 6

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -55,6 +55,16 @@ describe User do
     end
   end
 
+  describe "email address with mixed case" do
+    let (:mixed_case_email) { "F00@ExAMPLe.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
 
   describe "when email format is valid" do
     it "should be valid" do
