fc109941ee078571972534f93f9820f834f4f0a5
John Sakers <yzwy4mty9f@snkmail.com>
2012-05-28 04:15:08 -0400

    Through sidebar CSS

--- Gemfile
+++ Gemfile
@@ -32,5 +32,6 @@ group :test do
   gem 'libnotify', '0.5.9'
   gem 'guard-spork', '0.3.2'
   gem 'spork', '0.9.0'
+  gem 'factory_girl_rails', '1.4.0'
 end
 
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Pencil Cup Johnson"
+    email "pc@johnson.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
